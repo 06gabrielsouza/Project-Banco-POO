346259d5bd1ab9ce5959d3690b0eea0cb0072bbe
Gabriel Souza <163131961+06gabrielsouza@users.noreply.github.com>
2024-06-18 21:29:03 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente gabriel = new Cliente();
+        gabriel.setNome("Gabriel Souza");
+        Conta cc = new ContaCorrente(gabriel);
+        cc.depositar(100);
+
+        Conta poupança = new ContaPoupança(gabriel);
+
+        cc.transferir(100, poupança);
+        cc.imprimirExtrato();
+        poupança.imprimirExtrato();
+    }
+}
 
