f10a3f6afc6af5bd0515b92a9b89662156e0f002
Gabriel Souza <163131961+06gabrielsouza@users.noreply.github.com>
2024-06-18 21:26:06 -0300

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,11 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Cliente {
+    public String getNome() {
+        return nome;
     }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private String nome;
 }
